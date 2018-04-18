aacb06a09697884ab75e885541f5fd4b253e799c
Vegard Pihl Bratteng <mail@vebr.no>
2018-04-18 22:56:29 +0200

    Visual Studio Git Bug

--- ClassLibrary1/Dungeons/MovementSpeed/MoveModifier.cs
+++ ClassLibrary1/Dungeons/MovementSpeed/MoveModifier.cs
@@ -1,25 +1,26 @@
-﻿namespace ToolLibrary.Dungeons.Misc
+﻿namespace ToolLibrary.Dungeons.MovementSpeed
 {
     public class MovementSpeedModifier
     {
-        private int m_bonusSpeed;
-        private int m_duration { get; }
-        private int m_timeSpent { get; set; }
-
-        public bool NextTurn()
+        public readonly int BonusSpeed;
+        private int Duration { get; }
+        private int TimeSpent { get; set; }
+        public bool Active => Duration >= TimeSpent;
+        
+        public void NextTurn()
         {
-            return m_duration > m_timeSpent++;
+            TimeSpent++;
         }
 
         public int ReturnMovementModifier()
         {
-            return m_bonusSpeed;
+            return BonusSpeed;
         }
 
         public MovementSpeedModifier(int bonusSpeed, int duration)
         {
-            m_bonusSpeed = bonusSpeed;
-            m_duration = duration;
+            BonusSpeed = bonusSpeed;
+            Duration = duration;
         }
     }
 }
