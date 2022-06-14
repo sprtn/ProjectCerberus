3935a78e9e09bc249be0c09788f15092801410f5
Veaga <jompawars@gmail.com>
2022-06-14 20:41:19 +0200

    Added more configuration, added more averages and helper methods

--- NetworkAnalyser/Util/NetworkAnalyzer.cs
+++ NetworkAnalyser/Util/NetworkAnalyzer.cs
@@ -7,33 +7,55 @@ namespace NetworkService.Util
     {
         private readonly ILogger<Worker> Log;
         private readonly List<string> _hostsArr;
+        private Dictionary<string, List<int>> PingResultMsDictionary = new Dictionary<string, List<int>>();
+        private Dictionary<string, List<bool>> PingResultStatusDictionary = new Dictionary<string, List<bool>>();
         private readonly int _timeout;
+        public int SampleSize;
 
-        public NetworkAnalyzer(ILogger<Worker> logger, IConfiguration cfg)
+        public NetworkAnalyzer(ILogger<Worker> logger, IConfiguration cfg, int sampleSize)
         {
             Log = logger;
+            _hostsArr = new List<string>();
+            SampleSize = sampleSize;
+
             try
             {
                 _hostsArr = cfg.GetSection("Hosts").Get<List<string>>();
-                //_hostsArr = cfg.GetValue<string[]>("Hosts");
                 if (_hostsArr == null)
                     throw new ArgumentException("No hosts found: Configuration issue in appsettings");
                 if (_hostsArr.Count == 0)
                     throw new ArgumentException("No hosts found: Empty list.");
 
+                foreach (string host in _hostsArr)
+                {
+                    PingResultStatusDictionary.Add(host, new List<bool>());
+                    PingResultMsDictionary.Add(host, new List<int>());
+                }
+
                 _timeout = cfg.GetValue<int>("Timeout");
             }
             catch (Exception e)
             {
-                Log?.LogError(e, "Could not retrieve list of hosts from application configuration. Please verify Hosts under appsettings.");
+                Log.LogError(e, "Could not retrieve list of hosts from application configuration. Please verify Hosts under appsettings.");
             }
         }
 
         public async Task<List<(string host, IPStatus status, int ms)>> PingAllHosts()
         {
             List<(string host, IPStatus status, int ms)> responses = new();
-            foreach (var item in _hostsArr)
-                responses.Add(await PingHost(item));
+            foreach (var host in _hostsArr)
+            {
+                var result = await PingHost(host);
+                responses.Add(result);
+
+                if ((PingResultMsDictionary[host]?.Count ?? 0) > SampleSize)
+                    PingResultMsDictionary[host].RemoveAt(0);
+                PingResultMsDictionary[host].Add(result.ms);
+
+                if (PingResultStatusDictionary[host].Count > SampleSize)
+                    PingResultStatusDictionary[host].RemoveAt(0);
+                PingResultStatusDictionary[host].Add(result.status == IPStatus.Success);
+            }
 
             return responses;
         }
@@ -45,5 +67,18 @@ namespace NetworkService.Util
 
             return (host, reply.Status, (int)reply.RoundtripTime);
         }
+
+        public double GetAverageMSForHost(string host) => PingResultMsDictionary?[host]?.Average() ?? 0;
+        public double GetSuccessForHostPercentile(string host)
+        {
+            if (PingResultStatusDictionary.Count == 0)
+                return 0.5;
+
+            int numSuccess = PingResultStatusDictionary[host].Where(s => s.Equals(true)).Count();
+            double returnVal = (double)numSuccess / (double)PingResultStatusDictionary[host].Count;
+            return returnVal;
+        }
+
+        public int GetNumSamplesForHost(string host) => PingResultMsDictionary?[host]?.Count ?? 0;
     }
 }
--- NetworkAnalyser/Worker.cs
+++ NetworkAnalyser/Worker.cs
@@ -8,50 +8,52 @@ namespace NetworkService
         private readonly ILogger<Worker> Log;
         private readonly IConfiguration _cfg;
         private readonly ReleaseVersion _releaseVersion;
-        private NetworkAnalyzer Analyzer;
+        private readonly NetworkAnalyzer Analyzer;
 
         public Worker(ILogger<Worker> logger, IConfiguration cfg)
         {
             Log = logger;
             _cfg = cfg;
+            _releaseVersion = new ReleaseVersion();
 
             try
             {
                 var releaseVersion = _cfg.GetSection("Version");
                 _releaseVersion = new ReleaseVersion(releaseVersion.GetValue<int>("Major"), releaseVersion.GetValue<int>("Minor"));
             }
-            catch (Exception e)
+            catch
             {
-                /// TODO: Add smoother error handling with retry
-                Log.LogInformation("Fatal Error. Please verify appsettings configuration settings and restart the service.");
+                /// TODO: Could add smoother error handling with retry
+                Log.LogInformation("Error. Please verify appsettings configuration setting validity and restart the service to load settings properly.");
             }
             
-            Analyzer = new(logger, cfg);
+            Analyzer = new(logger, cfg, _cfg.GetValue<int>("SampleSize"));
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            Log.LogInformation("Running version {version}", _releaseVersion.ToString());
-            Log.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            Log.LogInformation($"Running version {_releaseVersion}");
+            Log.LogInformation($"Worker running at: {DateTimeOffset.Now}");
 
             while (!stoppingToken.IsCancellationRequested)
             {
                 if (DateTime.Now.Second % _cfg.GetValue<int>("RunInterval") == 0)
                 {
-                    Log.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    Log.LogInformation($"Worker running at: {DateTimeOffset.Now}");
                     foreach (var item in await Analyzer.PingAllHosts())
                     {
                         switch (item.status)
                         {
                             case System.Net.NetworkInformation.IPStatus.Success:
-                                Log.LogInformation($"Host {item.host} ping: {item.ms}ms");
+                                Log.LogInformation($"Host: {item.host}: Average MS over {Analyzer.GetNumSamplesForHost(item.host)} samples is {Analyzer.GetAverageMSForHost(item.host):0.00}. Average success rate is {(Analyzer.GetSuccessForHostPercentile(item.host) * 100):0.00}%");
                                 break;
                             default:
-                                Log.LogInformation($"Ping to Host {item.host} failed. Status: {item.status.ToString()}");
+                                Log.LogInformation($"Ping to Host {item.host} failed. Status: {item.status}. ");
                                 break;
                         }
 
                         /// Submit status to Loki / Prometheus statistics db
+
                     }
                 }
 
